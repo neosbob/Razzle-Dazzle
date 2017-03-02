0ef1e4207e778ceeca7c50125f6071124d1a45ec
bobbyhyz <bobbyhyz@gmail.com>
2017-03-02 14:29:57 -0800

    added integration test

--- integration.py
+++ integration.py
@@ -4,44 +4,92 @@ import urllib2
 from time import sleep
 import requests
 
+SERVER_CONFIG_NAME = "config_file"
+PROXY_CONFIG_NAME = "config_file_proxy"
+
 def threadExec():
-	subprocess.call(["./webserver", "config_file"])
+	subprocess.call(["./webserver", SERVER_CONFIG_NAME])
 
-print "Building binary"
-subprocess.call(["make"])
+def proxyThreadExec():
+    subprocess.call(["./webserver", PROXY_CONFIG_NAME])
 
-serverProcess = Process(target=threadExec)
-serverProcess.start()
+def serverTest():
+    print "Server integration test:"
+    print "Building binary"
+    subprocess.call(["make"])
 
-sleep(5)
+    serverProcess = Process(target=threadExec)
+    serverProcess.start()
 
-req = requests.get('http://localhost:3000')
+    sleep(5)
 
-if req.status_code != 200:
-	print "Error: Wrong Status Code."
-	exit(1)
-else:
-	print "Status Code Correct: 200"
+    req = requests.get('http://localhost:3000')
 
-if req.headers['content-type'] != 'text/plain':
-	print "Incorrect content-type"
-	exit(2)
-else:
-	print "Content Type Correct: text/plain"
+    if req.status_code != 200:
+	    print "Error: Wrong Status Code."
+	    exit(1)
+    else:
+	    print "Status Code Correct: 200"
 
-if req.headers['content-length'] != str(len(req.content)):
-	print "Error: Length Header Invalid"
-	exit(3)
-else: 
-	print "Content Length Correct"
+    if req.headers['content-type'] != 'text/plain':
+	    print "Incorrect content-type"
+	    exit(2)
+    else:
+	    print "Content Type Correct: text/plain"
 
-print "Integration Test Passed!"
+    if req.headers['content-length'] != str(len(req.content)):
+	    print "Error: Length Header Invalid"
+	    exit(3)
+    else: 
+	    print "Content Length Correct"
 
-print "Shutting down web server"
-subprocess.call(["fuser", "-k", "3000/tcp"])
+    print "Integration Test Passed!"
 
-print "Cleaning previous build"
-subprocess.call(["make", "clean"])
+    print "Shutting down web server"
+    subprocess.call(["fuser", "-k", "3000/tcp"])
 
-serverProcess.terminate()
-exit(0)
+    print "Cleaning previous build"
+    subprocess.call(["make", "clean"])
+
+    serverProcess.terminate()
+
+def proxyTest():
+    print "Proxy integration test:"
+    subprocess.call(["make"])
+
+    serverProcess = Process(target=threadExec)
+    serverProcess.start()
+    proxyProcess = Process(target=proxyThreadExec)
+    proxyProcess.start()
+
+    sleep(5)
+
+    req_server = requests.get('http://localhost:3000')
+    req_proxy = requests.get('http://localhost:3001')
+    
+
+    if req_server.status_code != 200 or req_proxy.status_code != 200:
+	    print "Error: Wrong Status Code."
+    else:
+	    print "Status Code Correct: 200"
+
+    print "\nContent of request to server is:"
+    print req_server.content
+    
+    print "Content of request to proxy is:"
+    print req_proxy.content
+
+    print "Shutting down web server"
+    subprocess.call(["fuser", "-k", "3000/tcp"])
+    subprocess.call(["fuser", "-k", "3001/tcp"])
+    subprocess.call(["make", "clean"])
+
+    serverProcess.terminate()
+    proxyProcess.terminate()
+
+def main():
+    serverTest()
+    proxyTest()
+    
+if __name__ == "__main__":
+    main()
