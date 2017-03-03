6ede2cd490a39272e18ebf1f34636d4758749ff7
bobbyhyz <bobbyhyz@gmail.com>
2017-03-02 18:44:29 -0800

    added integration test

--- integration_proxy.py
+++ integration_proxy.py
@@ -4,11 +4,7 @@ import urllib2
 from time import sleep
 import requests
 
-SERVER_CONFIG_NAME = "config_file"
 PROXY_CONFIG_NAME = "config_file_proxy"
-
-def threadExec():
-	subprocess.call(["./webserver", SERVER_CONFIG_NAME])
 
 def proxyThreadExec():
     subprocess.call(["./webserver", PROXY_CONFIG_NAME])
@@ -16,26 +12,22 @@ def proxyThreadExec():
 def proxyTest():
     print "Proxy integration test:"
     subprocess.call(["make"])
-
     proxyProcess = Process(target=proxyThreadExec)
     proxyProcess.start()
-    print "Hey!!!!!"
     sleep(0.01)
 
-    req_server = requests.get('http://my.ucla.edu')
+    req_server = requests.get('http://www.ucla.edu')
     req_proxy = requests.get('http://localhost:3001/')
-    
 
     if req_server.status_code != 200 or req_proxy.status_code != 200:
 	    print "Error: Wrong Status Code."
     else:
 	    print "Status Code Correct: 200"
 
-    
-    if req_server.content == req_proxy.content:
-        print "Proxy test passed!"
-    else:
-        print "Proxy test failed!"
+    print "Length of content from server is "
+    print len(req_server.content)
+    print "Length of content from proxy is "
+    print len(req_proxy.content)
 
     print "Shutting down web server"
     subprocess.call(["fuser", "-k", "3001/tcp"])
